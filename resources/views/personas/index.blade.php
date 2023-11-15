62611a9001e2e0a50d963857f3938811b273369b
GersonQuezada <101353607+GersonQuezada@users.noreply.github.com>
2023-11-15 12:19:04 -0500

    master

--- resources/views/personas/index.blade.php
+++ resources/views/personas/index.blade.php
@@ -4,16 +4,14 @@
 @stop
 
 @section('content')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
     <div class="container-fluid"></div>
-    <div class="card card-primary">
+    <div class="card card">
         <div class="card-header">
             <h3 class="card-title">Listado de Personal:</h3>
         </div>
         <div class="card-body">
-            <table id="ListadoPersonal" style="width:100%">
-                <thead>
+            <table id="articulos"class="table table-bordered table-hover" style="width:100%">
+                <thead class="bg-primary text-white">
                     <tr>
                         <th>DNI</th>
                         <th>Nombres</th>
@@ -22,30 +20,36 @@
                         <th>Estado</th>
                     </tr>
                 </thead>
-            </table>
+               
+            </table>            
+ 
         </div>
-    </div>
-
-    <script type="text/javascript">
-        $("#ListadoPersonal").dataTable({
-        ajax : {"cache": false,
-                "method":"GET",
-                "url":'{{URL('/Personas/ListadoPersonal')}}',
-                "dataType": "json",
-                "deferRender": true,},
-        columns: [
-                { data: 'name' },
-                { data: 'position' },
-                { data: 'office' },
-                { data: 'extn' },
-                { data: 'start_date' },
-                { data: 'salary' }
-            ]
-        });
-    </script>
-
-<script type="text/javascript" src="~/Scripts/jquery.js"></script>
-<script type="text/javascript" src="~/Scripts/data-table/jquery.dataTables.js"></script>
+    </div> 
 @stop
 
+@section('js')
 
+<script>
+$(document).ready(function() {
+    $('#articulos').DataTable({
+        ajax:{
+                "cache": false,
+                "method":"get",
+                "url": " {{  URL('/Personas/ListadoPersonal') }}",
+                "dataType": "json",
+                "deferRender": true,
+        },
+        columns:[
+            {"data" : "VC_DNI"},
+            {"data" : "VC_DNI"},
+            {"data" : "VC_DNI"},
+            {"data" : "VC_DNI"},
+            {"data" : "VC_DNI"}
+        ],
+        lengthMenu: [[5,10, 50, -1], [5, 10, 50, "All"]],
+        deferRender: true
+    });
+} );
+</script>
+
+@stop
