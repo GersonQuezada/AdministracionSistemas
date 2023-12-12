5d69e33bb2365f5f3cec59beb197d074558fdd4f
GersonQuezada <101353607+GersonQuezada@users.noreply.github.com>
2023-12-12 01:52:56 -0500

    master

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,17 +3,33 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h3>Panel Principal</h3>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>56</h3>
+                        <p>Usuarios Registrados</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion bi-person-add"></i>                    
+                    </div>
+                    <a href="{{ route('Personas.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    
 @stop
